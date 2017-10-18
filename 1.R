026b60b02e1e265a41f0ffd6062ec154f1c33890
zhehaoyu297 <32902840+zhehaoyu297@users.noreply.github.com>
2017-10-19 03:10:28 +0800

    Delete 1.R

--- 1.R
+++
@@ -1,3 +0,0 @@
-library(digest)
-digest("I learn a lot from this class when I am proper listening to the professor","sha256")
-digest("I do not learn a lot from this class when I am absent and playing on my Iphone","sha256")
